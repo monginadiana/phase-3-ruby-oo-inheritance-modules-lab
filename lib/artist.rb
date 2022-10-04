3c865358c9ab428329046ff8f131213f466adbd5
monginadiana <dianahmongina2@gmail.com>
2022-10-04 21:38:29 +0300

    addition of the namescaping in methods

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,12 @@
 require 'pry'
 
+require 'pry'
+require_relative '../config/environment'
+
 class Artist
   attr_accessor :name
   attr_reader :songs
-
+  extend Memorable
   @@artists = []
 
   def initialize
@@ -19,13 +22,6 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
 
   def add_song(song)
     @songs << song
